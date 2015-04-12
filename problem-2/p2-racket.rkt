3e603c3b801d663d6a1d1c2a9bf0701b943a2f82
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2015-04-12 14:04:47 -0600

    p2-racket - SOLVED

---
+++ problem-2/p2-racket.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+; First solution
+(define (even-fib-count start next)
+  (cond
+    [(> next 4000000) 0]
+    [(even? next) (+ next (even-fib-count next (+ start next)))]
+    [else (even-fib-count next (+ start next))]))
