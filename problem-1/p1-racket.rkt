8e002f1ef0b20842a12260c7b0caa8a87620ce25
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2015-04-12 10:29:06 -0600

    p1-racket created generic function

--- problem-1/p1-racket.rkt
+++ problem-1/p1-racket.rkt
@@ -8,8 +8,6 @@
     [(equal? 0 (modulo n 3)) (+ n (multiples-sum (- n 1)))]
     [else (multiples-sum (- n 1))]))
 
-;(multiples-sum 1000)
-
 ; more complicated solution
 ; still has same problems with large numbers
 (define (set-less-than val n)
@@ -43,3 +41,13 @@
 (define (multiples-sum-4 n)
   (+ (quick-sum-by 3 n) (quick-sum-by 5 n) (* -1 (quick-sum-by 15 n))))
   
+
+; Generic Solution
+(define (generic-multiples-sum n a b)
+  (+ (quick-sum-by a n) (quick-sum-by b n) (* -1 (quick-sum-by (* a b) n))))
+
+; Answer
+(generic-multiples-sum 999 3 5)
+
+; Proof of Large Number Concept
+(generic-multiples-sum 1000000000000000000000000000000000000000 3 5)
