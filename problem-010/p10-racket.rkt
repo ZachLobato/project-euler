6f85b83a3b6fb43238666fd6f78b4c9b2d581896
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2015-04-20 10:38:06 -0600

    Updated with different solution

--- problem-010/p10-racket.rkt
+++ problem-010/p10-racket.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require math/number-theory)
+(require test-engine/racket-tests)
 
 
 (define (sum-primes n)
@@ -10,10 +11,16 @@
         (+ cur-prime (iter (next-prime cur-prime)))))
   (iter 2))
 
+(time (apply + (next-primes 1 148933)))
+
 ;cpu time: 0 real time: 1 gc time: 0
-(time (sum-primes 10))
+;(time (sum-primes 10))
 
 ;cpu time: 7878 real time: 7954 gc time: 1531
-(time (sum-primes 2000000))
+;(time (sum-primes 2000000))
 
 ;simple, easy, and effective
+
+(check-expect (sum-primes 10) 17)
+(test)
+
